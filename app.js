4d34aed4c948004c018d78ac4a9710897f807e7d
Remie Bolte <r.bolte@gmail.com>
2015-08-24 18:43:27 +0200

    #41 Add support for sending tokens by email Fixing indentation

--- app.js
+++ app.js
@@ -78,9 +78,9 @@ app.use(function(req, res, next) {
 			var template = app.viewEngine.getTemplate(name);
 			if(template.path.match(/\/views\/pages\//)) {
 				var output = nunjucks.render(name, context, callback);
-					res.setHeader('Content-type', 'text/html');
-					res.writeHead(200);
-					res.end(output);
+				res.setHeader('Content-type', 'text/html');
+				res.writeHead(200);
+				res.end(output);
 			} else {
 				throw new Error("The selected template is not a page, throwing 'template not found' error for proper handling");
 			}
@@ -88,18 +88,18 @@ app.use(function(req, res, next) {
 			if(err.message.match(/template not found/)) {
 				try {
 					var output = nunjucks.render("404.html");
-						res.setHeader('Content-type', 'text/html');
-						res.writeHead(404);
-						res.end(output);
+					res.setHeader('Content-type', 'text/html');
+					res.writeHead(404);
+					res.end(output);
 				} catch(err) {
-						res.writeHead(404);
-						res.end("Resource not found");
-					}
-				} else {
-					res.writeHead(500);
-					res.end(err.message);
+					res.writeHead(404);
+					res.end("Resource not found");
 				}
+			} else {
+				res.writeHead(500);
+				res.end(err.message);
 			}
+		}
 	};
 	next();
 });
