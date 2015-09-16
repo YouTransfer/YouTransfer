afdaa63ec1bae405e35ba4f3c61abd99565f6d6f
Remie Bolte <r.bolte@gmail.com>
2015-09-16 19:52:26 +0200

    fixes #78 : Add UnitTests for modules Making adjustments to scheduler module to enable unit testing

--- test/modules/settings.test.js
+++ test/modules/settings.test.js
@@ -1,21 +1,31 @@
 
+// ------------------------------------------------------------------------------------------ Test Dependencies
+
 var should = require('chai').should();
 var settings = require('../../lib/settings.js')({
 	path: './test/modules/settings.json'
 });
 
+// ------------------------------------------------------------------------------------------ Test Definition
+
 describe('YouTransfer Settings module', function() {
 	var title = "My Awesome Title";
+
+	// -------------------------------------------------------------------------------------- Testing constructor
 
 	it('should be possible to set options', function() {
 		settings.options.path.should.equal('./test/modules/settings.json');
 	});
+
+	// -------------------------------------------------------------------------------------- Testing write
 
 	it('should be possible to set title', function() {
 		settings.push({ title: title }, function(err) {
 			should.not.exist(err);
 		});
 	});
+
+	// -------------------------------------------------------------------------------------- Testing read
 
 	it('should be possible to get title', function() {
 		settings.get(function(err, output) {
