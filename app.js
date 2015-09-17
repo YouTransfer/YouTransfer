757c5f93ae8b48c4d8eeec0efe2072f8eda61629
Remie Bolte <r.bolte@gmail.com>
2015-09-17 15:02:53 +0200

    fixes #78 : Add UnitTests for modules Refactoring modules in preparation of unit test creation

--- app.js
+++ app.js
@@ -12,109 +12,42 @@ nconf.set('basedir', __dirname);
 
 // ------------------------------------------------------------------------------------------ App Dependencies
 
-var _ = require("lodash");
-var scheduler = require('./lib/scheduler.js');
-var youtransfer = require('./lib/youtransfer.js');
-
-// Restify + node-static + proxy
-var nstatic = require('node-static');
-var compression = require('compression')
-var nunjucks = require("nunjucks");
+// Restify
 var restify = require('restify');
 restify.cookieParser = require('restify-cookies');
+restify.compression = require('compression');
+
+// YouTransfer
+var routes = require('./lib/routes');
+var middleware = require('./lib/middleware');
+var youtransfer = require('./lib/youtransfer');
 
 // ------------------------------------------------------------------------------------------ App Initialization
 
 var app = restify.createServer(); 
-app.static = new nstatic.Server('./dist');
 app.pre(restify.pre.sanitizePath());
 app.use(restify.bodyParser());
 app.use(restify.queryParser());
 app.use(restify.cookieParser.parse);
-app.use(compression());
-
-// Fix for missing redirect function in Restify
-app.use(function(req,res,next) {
-	res.redirect = function(addr) { 
-		res.header('Location', addr); 
-		res.send(302); 
-	} 
-	next();
-});
-
-// Add XmlHttpRequest property to request object
-app.use(function(req, res, next) {
-	req.isXmlHtppRequest = (req.headers['x-requested-with'] && req.headers['x-requested-with'] == 'XMLHttpRequest');
-	next();
-});
-
-// Initializing Nunjucks template engine + adding it to Restify
-app.viewEngine = nunjucks.configure(['src/views/', 'src/views/partials', 'src/views/pages', 'src/views/errors', 'src/templates/'], {
-	autoescape: true,
-	watch: (nconf.get('NODE_ENV') != "production")
-});
-
-app.use(function(req, res, next) {
-	res.renderTemplate = function(name, context, callback) {
-		youtransfer.settings.get(function(err, settings) {
-			context = err ? context : _.assign(settings, context);
-			nunjucks.render(name, context, callback);
-		});
-	};
-
-	res.render = function(name, context, callback) {
-		res.setHeader('Server', 'youtransfer.io');
-
-		youtransfer.settings.get(function(err, settings) {
-			context = err ? context : _.assign(settings, context);
-			context.isXmlHtppRequest = req.isXmlHtppRequest;
-
-			try {
-				var template = app.viewEngine.getTemplate(name);
-				if(template.path.match(/\/views\/pages\//)) {
-					var output = nunjucks.render(name, context, callback);
-					res.setHeader('Content-type', 'text/html');
-					res.writeHead(200);
-					res.end(output);
-				} else {
-					throw new Error("The selected template is not a page, throwing 'template not found' error for proper handling");
-				}
-			} catch (err) {
-				if(err.message.match(/template not found/)) {
-					try {
-						var output = nunjucks.render("404.html", context);
-						res.setHeader('Content-type', 'text/html');
-						res.writeHead(404);
-						res.end(output);
-					} catch(err) {
-						res.writeHead(404);
-						res.end("Resource not found");
-					}
-				} else {
-					res.writeHead(500);
-					res.end(err.message);
-				}
-			}
-		});
-	};
-	next();
-});
+app.use(restify.compression());
+app.use(middleware);
 
 // ------------------------------------------------------------------------------------------ App Routing
 
-// Initialize routes
-require('./lib/routes')(app, nconf);
-
-// ------------------------------------------------------------------------------------------ App Scheduling
-
-youtransfer.settings.get(function(err, settings) {
-	if(!err) {
-		scheduler.add('cleanup', settings.cleanupSchedule, youtransfer.cleanup);
-		youtransfer.settings.on('settings.push', function(err, data) {
-			scheduler.reschedule('cleanup', data.cleanupSchedule, youtransfer.cleanup);
-		});
-	}
-});
+app.post('/upload', routes.upload);
+app.post('/upload/bundle', routes.uploadBundle);
+app.post(/^\/send/, routes.send);
+app.get('/download/:token', routes.downloadFile);
+app.post(/^\/download/, routes.downloadFile);
+app.get('/bundle/:token', routes.downloadBundle);
+app.get('/settings', routes.settingsRedirect);
+app.post('/settings/finalise', routes.settingsFinalise);
+app.get('/settings/template/:name', routes.settingsGetTemplateByName);
+app.post('/settings/template/', routes.settingsSaveTemplate);	
+app.get('/settings/:name', routes.settingsGetByName);
+app.post('/settings/:name', routes.settingsSaveByname);
+app.get(/^(\/v\d*)?\/(js|css|assets|fonts|img|sounds)\/(.*)/, routes.staticFiles);
+app.get(/^\/(.*)/, routes.default);
 
 // ------------------------------------------------------------------------------------------ App Execution
 
@@ -123,5 +56,3 @@ var port = Number(nconf.get('PORT'));
 app.listen(port, function() {
 	console.log('%s listening at %s', app.name, app.url);
 });
-
-
