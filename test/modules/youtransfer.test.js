8ed6c40c6880c331bab5634d422f34e39d3f51c9
Remie Bolte <r.bolte@gmail.com>
2015-09-20 18:03:46 +0200

    fixes #78 : Add UnitTests for modules Adding unit tests for YouTransfer module

---
+++ test/modules/youtransfer.test.js
@@ -0,0 +1,609 @@
+
+// ------------------------------------------------------------------------------------------ Test Dependencies
+
+var sinon = require('sinon');
+var should = require('chai').should();
+var filesize = require("filesize");
+var path = require('path');
+var youtransfer = require('../../lib/youtransfer');
+
+// ------------------------------------------------------------------------------------------ Mock Dependencies
+
+var scheduler = require('../../lib/scheduler');
+var nodemailer = require('nodemailer');
+
+// ------------------------------------------------------------------------------------------ Test Definition
+
+describe('YouTransfer module', function() {
+	var sandbox;
+
+	beforeEach(function() {
+		sandbox = sinon.sandbox.create();
+	});
+
+	afterEach(function() {
+		sandbox.restore();
+	})
+
+	// -------------------------------------------------------------------------------------- Testing initialize
+
+	it('should be possible to initialize module (incl. scheduling of background jobs)', function(done) {
+
+		var settings = {
+			schedulerEnabled: true,
+			cleanupSchedule: 'cronschedule',
+			on: function() {}
+		}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+
+		sandbox.stub(youtransfer.settings, "on", function(event, callback) {
+			callback(null, settings);
+		});
+
+		sandbox.stub(scheduler, "add", function(name, schedule, job) {
+			name.should.equals('cleanup');
+			schedule.should.equals(settings.cleanupSchedule);
+			should.exist(job);
+		});
+
+		sandbox.stub(scheduler, "reschedule", function(name, schedule, job) {
+			name.should.equals('cleanup');
+			schedule.should.equals(settings.cleanupSchedule);
+			should.exist(job);
+			done();
+		});
+
+		youtransfer.initialize();
+	});
+
+	it('should continue if an error occurs while initializing module', function() {
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(new Error('error'), null);
+		});
+
+		youtransfer.initialize();
+	});
+
+
+
+	// -------------------------------------------------------------------------------------- Testing storageFactory
+
+	it('should be possible to retrieve the default storage provider from the storage factory', function(done) {
+
+		var settings = {}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+
+		youtransfer.storageFactory.get(function(err, factory) {
+			should.not.exist(err);
+			should.exist(factory);
+			done();
+		});
+
+	});
+
+	it('should be possible to retrieve the local storage provider from the storage factory', function(done) {
+
+		var settings = {
+				StorageLocation: 'local'
+			}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+
+		youtransfer.storageFactory.get(function(err, factory) {
+			should.not.exist(err);
+			should.exist(factory.options.StorageLocation);
+			factory.options.StorageLocation.should.equals(settings.StorageLocation);
+			done();
+		});
+
+	});
+
+	it('should be possible to retrieve the Amazon S3 storage provider from the storage factory', function(done) {
+
+		var settings = {
+				StorageLocation: 's3'
+			}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+
+		youtransfer.storageFactory.get(function(err, factory) {
+			should.not.exist(err);
+			should.exist(factory.options.StorageLocation);
+			factory.options.StorageLocation.should.equals(settings.StorageLocation);
+			done();
+		});
+
+	});
+
+	it('should continue with erronous callback if an error occurs while retrieving the storage provider from the storage factory', function(done) {
+
+		var settings = {
+			StorageLocation: 'unknown'
+		}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+
+		youtransfer.storageFactory.get(function(err, factory) {
+			should.exist(err);
+			should.not.exist(factory);
+			err.message.should.equals('Storage provider not supported');
+			done();
+		});
+
+	});
+
+	// -------------------------------------------------------------------------------------- Testing upload
+
+	it('should be possible to upload a file', function(done) {
+
+		var file = {
+				id: 'file',
+				name: 'file',
+				type: 'file',
+				size: 1024,
+				lastModifiedDate: 'now'
+			},
+			bundle = 'bundle',
+			settings = {
+				localstoragepath: __dirname,
+				retentionUnit: 'seconds',
+				retention: 1
+			},
+			factory = {
+				upload: function() {}
+			};
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+		sandbox.stub(youtransfer.storageFactory, "get", function(callback) {
+			callback(null, factory);
+		});
+		sandbox.stub(factory, "upload", function(file, context, callback) {
+			should.exist(file);
+			should.exist(context);
+			context.size.should.equals(file.size);
+			context.filesize.should.equals(filesize(file.size));
+			context.type.should.equals(file.type)
+			context.lastModifiedDate.should.equals(file.lastModifiedDate);
+			callback();
+		});
+
+		youtransfer.upload(file, bundle, function() {
+			done();
+		});
+	});
+
+	it('should be possible to upload a file without retention', function(done) {
+
+		var file = {
+				id: 'file',
+				name: 'file',
+				type: 'file',
+				size: 1024,
+				lastModifiedDate: 'now'
+			},
+			bundle = 'bundle',
+			settings = {
+				localstoragepath: __dirname,
+				retentionUnit: 'seconds'
+			},
+			factory = {
+				upload: function() {}
+			};
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+		sandbox.stub(youtransfer.storageFactory, "get", function(callback) {
+			callback(null, factory);
+		});
+		sandbox.stub(factory, "upload", function(file, context, callback) {
+			should.exist(file);
+			should.exist(context);
+			context.size.should.equals(file.size);
+			context.filesize.should.equals(filesize(file.size));
+			context.type.should.equals(file.type)
+			context.lastModifiedDate.should.equals(file.lastModifiedDate);
+			context.expires.should.equals(false);
+			callback();
+		});
+
+		youtransfer.upload(file, bundle, function() {
+			done();
+		});
+	});	
+
+	// -------------------------------------------------------------------------------------- Testing bundle
+
+	it('should be possible to upload a bundle', function(done) {
+
+		var bundle = {
+				id: 'bundle'
+			}
+			settings = {
+				localstoragepath: __dirname,
+				retentionUnit: 'seconds',
+				retention: 1
+			},
+			factory = {
+				bundle: function() {}
+			};
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+		sandbox.stub(youtransfer.storageFactory, "get", function(callback) {
+			callback(null, factory);
+		});
+		sandbox.stub(factory, "bundle", function(context, callback) {
+			should.exist(context);
+			context.id.should.equals(bundle.id);
+			context.path.should.equals(path.join(__dirname, bundle.id + '.json'));
+			callback();
+		});
+
+		youtransfer.bundle(bundle, function() {
+			done();
+		});
+	});
+
+	it('should be possible to upload a bundle without retention', function(done) {
+
+		var bundle = {
+				id: 'bundle'
+			}
+			settings = {
+				localstoragepath: __dirname,
+				retentionUnit: 'seconds',
+			},
+			factory = {
+				bundle: function() {}
+			};
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+		sandbox.stub(youtransfer.storageFactory, "get", function(callback) {
+			callback(null, factory);
+		});
+		sandbox.stub(factory, "bundle", function(context, callback) {
+			should.exist(context);
+			context.id.should.equals(bundle.id);
+			context.path.should.equals(path.join(__dirname, bundle.id + '.json'));
+			context.expires.should.equals(false);
+			callback();
+		});
+
+		youtransfer.bundle(bundle, function() {
+			done();
+		});
+	});
+
+	// -------------------------------------------------------------------------------------- Testing download
+
+	it('should be possible to download a file', function(done) {
+
+		var token = 'file',
+			factory = {
+				download: function() {}
+			};
+
+		sandbox.stub(youtransfer.storageFactory, "get", function(callback) {
+			callback(null, factory);
+		});
+		sandbox.stub(factory, "download", function(file, res, callback) {
+			should.exist(token);
+			file.should.equals(token)
+			callback();
+		});
+
+		youtransfer.download(token, null, function() {
+			done();
+		});
+	});
+
+	// -------------------------------------------------------------------------------------- Testing archive
+
+	it('should be possible to download a bundle', function(done) {
+
+		var token = 'file',
+			factory = {
+				archive: function() {}
+			};
+
+		sandbox.stub(youtransfer.storageFactory, "get", function(callback) {
+			callback(null, factory);
+		});
+		sandbox.stub(factory, "archive", function(file, res, callback) {
+			should.exist(token);
+			file.should.equals(token)
+			callback();
+		});
+
+		youtransfer.archive(token, null, function() {
+			done();
+		});
+	});
+
+	// -------------------------------------------------------------------------------------- Testing cleanup
+
+	it('should be possible to trigger a purge', function(done) {
+
+		var factory = {
+				purge: function() {}
+			};
+
+		sandbox.stub(youtransfer.storageFactory, "get", function(callback) {
+			callback(null, factory);
+		});
+		sandbox.stub(factory, "purge", function(callback) {
+			callback(null, [ 'file' ]);
+		});
+
+		youtransfer.cleanup(function(err, files) {
+			should.exist(files);
+			files[0].should.equals('file');
+			done();
+		});
+	});
+
+	// -------------------------------------------------------------------------------------- Testing send
+
+	// THIS TEST IS NOT REALLY DOING ANYTHING OTHER THAN CONFIRMING THAT THE LINES WHERE CALLED AND CONTROL FLOW FOLLOWED
+	// TODO: add checks to all stub calls to verify if email creation is done correctly
+
+	it('should be possible to send an email using smtp with known service', function(done) {
+
+		var req = {
+				params: {
+					email: {
+						to: 'novalidemail'
+					},
+					files: {
+							0: encodeURIComponent(JSON.stringify({ id: 'file' }))
+					}
+				}
+			},
+			res = {
+				renderTemplate: function() {}
+			},
+			settings = {
+				email: {
+					service: 'gmail',
+					transporter: 'smtp'
+				}
+			},
+			transporter = {
+				sendMail: function() {}
+			}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+		sandbox.stub(nodemailer, "createTransport").returns(transporter);
+		sandbox.stub(transporter, "sendMail", function(email, callback) {
+			callback();
+		});
+		sandbox.stub(res, 'renderTemplate', function(template, fields, callback) {
+			callback(null, 'body');
+		});
+
+		youtransfer.send(req, res, function() {
+			done();
+		});
+	});
+
+	it('should be possible to send an email using smtp with unknown service', function(done) {
+
+		var req = {
+				params: {
+					email: {
+						to: 'novalidemail'
+					}
+				}
+			},
+			res = {
+				renderTemplate: function() {}
+			},
+			settings = {
+				email: {
+					service: '',
+					transporter: 'smtp'
+				}
+			},
+			transporter = {
+				sendMail: function() {}
+			}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+		sandbox.stub(nodemailer, "createTransport").returns(transporter);
+		sandbox.stub(transporter, "sendMail", function(email, callback) {
+			callback();
+		});
+		sandbox.stub(res, 'renderTemplate', function(template, fields, callback) {
+			callback(null, 'body');
+		});
+
+		youtransfer.send(req, res, function() {
+			done();
+		});
+	});
+
+	it('should be possible to send an email using sendmail', function(done) {
+
+		var req = {
+				params: {
+					email: {
+						to: 'novalidemail'
+					}
+				}
+			},
+			res = {
+				renderTemplate: function() {}
+			},
+			settings = {
+				email: {
+					transporter: 'sendmail'
+				}
+			},
+			transporter = {
+				sendMail: function() {}
+			}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+
+		sandbox.stub(nodemailer, "createTransport").returns(transporter);
+
+		sandbox.stub(res, 'renderTemplate', function(template, fields, callback) {
+			callback(null, 'body');
+		});
+
+		sandbox.stub(transporter, "sendMail", function(email, callback) {
+			callback();
+		});
+
+		youtransfer.send(req, res, function() {
+			done();
+		});
+	});	
+
+	it('should be possible to send an email using Amazon SES', function(done) {
+
+		var req = {
+				params: {
+					email: {
+						to: 'novalidemail'
+					}
+				}
+			},
+			res = {
+				renderTemplate: function() {}
+			},
+			settings = {
+				email: {
+					transporter: 'ses'
+				}
+			},
+			transporter = {
+				sendMail: function() {}
+			}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+
+		sandbox.stub(nodemailer, "createTransport").returns(transporter);
+
+		sandbox.stub(res, 'renderTemplate', function(template, fields, callback) {
+			callback(null, 'body');
+		});
+
+		sandbox.stub(transporter, "sendMail", function(email, callback) {
+			callback();
+		});
+
+		youtransfer.send(req, res, function() {
+			done();
+		});
+	});	
+
+	it('should be possible to send an email using direct MX connection', function(done) {
+
+		var req = {
+				params: {
+					email: {
+						to: 'novalidemail'
+					}
+				}
+			},
+			res = {
+				renderTemplate: function() {}
+			},
+			settings = {
+				email: {
+					transporter: 'notsupported'
+				}
+			},
+			transporter = {
+				sendMail: function() {}
+			}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+
+		sandbox.stub(nodemailer, "createTransport").returns(transporter);
+
+		sandbox.stub(res, 'renderTemplate', function(template, fields, callback) {
+			callback(null, 'body');
+		});
+
+		sandbox.stub(transporter, "sendMail", function(email, callback) {
+			callback();
+		});
+
+		youtransfer.send(req, res, function(err) {
+			done();
+		});
+	});	
+
+	it('should continue with erronous callback if an error occurs while generating the email template', function(done) {
+
+		var req = {
+				params: {
+					email: {
+						to: 'novalidemail'
+					}
+				}
+			},
+			res = {
+				renderTemplate: function() {}
+			},
+			settings = {
+				email: {
+					transporter: 'notsupported'
+				}
+			},
+			transporter = {
+				sendMail: function() {}
+			}
+
+		sandbox.stub(youtransfer.settings, "get", function(callback) {
+			callback(null, settings);
+		});
+
+		sandbox.stub(nodemailer, "createTransport").returns(transporter);
+
+		sandbox.stub(res, 'renderTemplate', function(template, fields, callback) {
+			callback(new Error('error'), null);
+		});
+
+		sandbox.stub(transporter, "sendMail", function(email, callback) {
+			callback();
+		});
+
+		youtransfer.send(req, res, function(err) {
+			should.exist(err);
+			err.message.should.equals('error');
+			done();
+		});
+	});	
+
+});
