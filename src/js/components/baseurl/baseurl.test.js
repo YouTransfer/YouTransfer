bd91d98317706a694e115df76b88df74cbb16317
Remie Bolte <r.bolte@gmail.com>
2015-09-21 16:43:29 +0200

    fixes #79 : Add UnitTests for components Adding unit tests for BaseUrl component

---
+++ src/js/components/baseurl/baseurl.test.js
@@ -0,0 +1,35 @@
+// Unit test for MODULE_NAME component
+// Mocha and Chai are always included
+'use strict';
+
+var $ = require('jquery');
+var baseurl = require('./baseurl');
+
+describe('BaseUrl component', function() {
+
+	it('should not add warning to document.body if base URL matches window.location.href', function() {
+
+		var fixture = getFixture(window.location.href);
+		var instance = new baseurl(fixture);
+
+		$('.baseurl').length.should.equals(0);
+
+	});
+
+	it('should add warning to document.body if base URL does not match window.location.href', function() {
+
+		var fixture = getFixture('http://someurl');
+		var instance = new baseurl(fixture);
+
+		$('.baseurl').html().should.equals("<strong>Warning</strong>: the base URL is set to someurl but you are accessing this page from localhost:5000. It is recommended to update the BaseUrl setting.");
+
+	});
+
+});
+
+function getFixture(value) {
+	var fixture = document.createElement('meta');
+	fixture.setAttribute('name', 'baseurl');
+	fixture.setAttribute('value', value);
+	return fixture;
+}
