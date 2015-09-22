66a357bf4790468cc3e19d60f66eb96736a0dba3
Remie Bolte <r.bolte@gmail.com>
2015-09-22 02:23:51 +0200

    fixes #80 : Write UI tests for important views Removing the Views tests from Karma configuration as they are meant to be executed with WebDriverIO instead of Karma

--- test/components/karma.conf.js
+++ test/components/karma.conf.js
@@ -14,7 +14,6 @@ module.exports = function (config) {
         //list of files / patterns to load in the browser
         files: [
             { pattern: 'src/js/components/**/*.test.js' },
-            { pattern: 'test/views/**/*.test.js' }
         ],
 
         // preprocess matching files before serving them to the browser
