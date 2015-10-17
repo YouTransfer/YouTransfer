b436eb8ece2d25fb9db02c51d0f6dfeee6f5df29
Remie Bolte <r.bolte@gmail.com>
2015-10-17 22:56:13 +0200

    Disable default click behaviour

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 $(function() {
-	$('a').click(function() {
+	$('a').click(function(event) {
+		event.preventDefault();
 		var target = this.getAttribute('href');
 		$(window).scrollTo($(target), 1000);
 	});
